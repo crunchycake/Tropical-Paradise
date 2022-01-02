02ee40bd1587fcb90f48e0192f2f58998003152a
Mateusz Cieslik <mateuszcieslik@gmail.com>
2022-01-02 15:16:25 +0100

    added dreams__box

--- js/main.js
+++ js/main.js
@@ -10,6 +10,20 @@ const handleNav = () => {
 			nav.classList.remove('nav--active')
 		})
 	})
+
+    handleNavItemsAnimation();
 }
+
+const handleNavItemsAnimation = () => {
+    let delayTime = 0;
+
+    allNavItems.forEach(item  => {
+        item.classList.toggle('nav-items-animation')
+        item.style.animationDelay = '.' + delayTime + 's';
+        delayTime++;
+    })
+}
+
+
 
 navBtn.addEventListener('click', handleNav)
